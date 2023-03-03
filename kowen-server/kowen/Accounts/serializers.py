ea07ef4507d71be4881e710dee29056f20dadb4f
AntonStankov <antonstankov007@gmail.com>
2023-03-03 20:06:53 +0200

    added registration

--- kowen-server/kowen/Accounts/serializers.py
+++ kowen-server/kowen/Accounts/serializers.py
@@ -6,17 +6,26 @@ from rest_framework.authtoken.models import Token
 from rest_framework import serializers
 from .models import User, Role
 
+
+from rest_framework import serializers
+from .models import User, Role
+
+
 class RoleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Role
         fields = '__all__'
 
+
 class UserSerializer(serializers.ModelSerializer):
-    roles = RoleSerializer(many=True, read_only=True)
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password', 'email',
+                  'first_name', 'last_name', 'roles', 'created_at')
+        fields = ('id', 'username', 'password', 'email',
+                  'first_name', 'last_name', 'roles')
 
     class Meta:
         model = User
-        fields = ('id', 'username','password', 'email', 'first_name', 'last_name', 'roles', 'created_at')
-
-
-
+        fields = ('id', 'username', 'password', 'email',
+                  'first_name', 'last_name', 'roles', 'created_at')
