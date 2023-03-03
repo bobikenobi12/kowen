e3f03e9dc5d23508497b30999381fa76250217d7
AntonStankov <antonstankov007@gmail.com>
2023-03-03 22:56:02 +0200

    hashing password

--- kowen-server/kowen/Accounts/views.py
+++ kowen-server/kowen/Accounts/views.py
@@ -4,6 +4,12 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import UserSerializer
+from django.contrib.auth import authenticate
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import bcrypt
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -17,7 +23,14 @@ class CreateRoleView(generics.CreateAPIView):
 @api_view(['POST'])
 def register_user(request):
     serializer = UserSerializer(data=request.data)
+
     if serializer.is_valid():
+
+        password = serializer.validated_data.get("password").encode('utf-8')
+        hashedPassword = bcrypt.hashpw(password, bcrypt.gensalt())
+        serializer.validated_data["password"] = hashedPassword
+        print(serializer.validated_data.get("password"))
+
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
