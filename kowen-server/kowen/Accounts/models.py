c1d27a6c48e349beb1ee72acfffa51ea420161c1
Borislav Borisov <borislav1207@gmail.com>
2023-03-14 14:27:47 +0200

    created Document model but still thinking how we should store the documents

--- kowen-server/kowen/Accounts/models.py
+++ kowen-server/kowen/Accounts/models.py
@@ -7,43 +7,41 @@ from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import Group
+import base64
 
-
-class Role(models.Model):
-    name = models.CharField(max_length=30, unique=True)
-    description = models.TextField(max_length=500, blank=True)
-    role_picture = models.ImageField(upload_to='role_pictures/', blank=True)
-    role_color = models.CharField(max_length=30, blank=True)
-    role_icon = models.CharField(max_length=30, blank=True)
-    ROLE_PERMISSIONS = (
-        ('can_view', 'can_view'),
-        ('can_edit', 'can_edit'),
-        ('can_delete', 'can_delete'),
-        ('can_create', 'can_create'),
-        ('can_share', 'can_share'),
-        ('can_download', 'can_download'), 
-        ('can_upload', 'can_upload'),
-    )
-    role_permissions = models.CharField(
-        max_length=30, blank=True, choices=ROLE_PERMISSIONS)
-
-    def __str__(self):
-        return self.name
+# create a document model that holds information about a document liek type, metadata, etc. The document is stored in the database
 
 
 class User(AbstractUser):
-
-
-    username = models.CharField(max_length=30, unique=True)
     email = models.EmailField(_('email address'), unique=True)
+    password = models.CharField(max_length=100)
+    username = models.CharField(max_length=30, unique=True)
     first_name = models.CharField(max_length=30, blank=True)
-    password = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=30, blank=True)
-    # bio = models.CharField(max_length=30, blank=True)   
-    REQUIRED_FIELDS = ["password"]
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
 
-     
+
+class Document(models.Model):
+    _document = models.TextField(
+        blank=True,
+        null=True,
+    )
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True)
+    description = models.TextField(blank=True)
+    author = models.ManyToOneRel(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # create 2 methods: set_document and get_document. Set document will take a file and convert it to a string, encode it with base64, hash it with bcrypt and store it in the database. Get document will take the string, decode it with base64, and return the file\
+
+    def set_document(self, document):
+        self._document = base64.b64encode(document)
+
+    def get_document(self):
+        return base64.b64decode(self._document)
+
     def __str__(self):
-        return self.email
-
-
+        return self.title
