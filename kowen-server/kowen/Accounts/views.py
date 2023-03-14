ffe1dce2f151a9926a30a318a4f619f7399f785b
Borislav Borisov <borislav1207@gmail.com>
2023-03-14 14:49:40 +0200

    parsing info

--- kowen-server/kowen/Accounts/views.py
+++ kowen-server/kowen/Accounts/views.py
@@ -1,15 +1,19 @@
 from rest_framework import generics
 from .serializers import *
 from .forms import RegistrationForm
+import json
+
 
 class CreateUserView(generics.CreateAPIView):
-    serializer_class = UserSerializer 
+    serializer_class = UserSerializer
+
 
 class CreateRoleView(generics.CreateAPIView):
     serializer_class = RoleSerializer
 
+
 def signup(request):
     if request.method == 'POST':
-        form = RegistrationForm(request.POST)
+        json.parse(request.body)
     else:
         form = RegistrationForm()
