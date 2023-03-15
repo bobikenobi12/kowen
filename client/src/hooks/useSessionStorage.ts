629422d28181b79fd8111b6efe62e8356e08563d
bobikenobi12 <borislav1207@gmail.com>
2023-03-15 22:42:11 +0200

    created session storage hook

---
+++ client/src/hooks/useSessionStorage.ts
@@ -0,0 +1,36 @@
+// create a custom hook to use session storage with typescript typings and redux toolkit
+
+import { useState, useEffect } from "react";
+
+export const useSessionStorage = <T>(key: string, initialValue: T) => {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try {
+      const item = window.sessionStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value: T | ((val: T) => T)) => {
+    const valueToStore = value instanceof Function ? value(storedValue) : value;
+    try {
+      const valueToStoreString = JSON.stringify(valueToStore);
+      setStoredValue(valueToStore);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const valueToStoreString = JSON.stringify(storedValue);
+    window.sessionStorage.setItem(key, valueToStoreString);
+
+    return () => {
+      window.sessionStorage.removeItem(key);
+    };
+  }, [storedValue]);
+
+  return [storedValue, setValue];
+};
