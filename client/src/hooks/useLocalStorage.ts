75992b00445d4fbd5b8e68dc6f04e44a3da1daa0
bobikenobi12 <borislav1207@gmail.com>
2023-03-15 23:03:06 +0200

    changed from session storage to local storage

---
+++ client/src/hooks/useLocalStorage.ts
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+export const uselocalStorage = <T>(key: string, initialValue: T) => {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value: T | ((val: T) => T)) => {
+    const valueToStore = value instanceof Function ? value(storedValue) : value;
+    try {
+      setStoredValue(valueToStore);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const valueToStoreString = JSON.stringify(storedValue);
+    window.localStorage.setItem(key, valueToStoreString);
+
+    return () => {
+      window.localStorage.removeItem(key);
+    };
+  }, [storedValue]);
+
+  return [storedValue, setValue];
+};
