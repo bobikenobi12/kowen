ff41b665898df3adc8bc33bc087f17a23412516a
bobikenobi12 <borislav1207@gmail.com>
2023-03-16 21:25:26 +0200

    added types for services and made an rtk query for registering, logging, logging out and selecting users

---
+++ client/src/app/api/types/User.ts
@@ -0,0 +1,13 @@
+// user types for api
+
+export interface User {
+    id: string;
+    first_name: string;
+    last_name: string;
+    email: string;
+    avatar: string;
+    _token: string;
+    _documents: never[];
+}
+
+
