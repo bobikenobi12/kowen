225610785a961c53d0f439f8017299d1d4c28bfe
AntonStankov <antonstankov007@gmail.com>
2023-03-18 17:12:56 +0200

    initialized Spring project

---
+++ Kowen/src/main/java/com/example/Kowen/KowenApplication.java
@@ -0,0 +1,13 @@
+package com.example.Kowen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KowenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KowenApplication.class, args);
+	}
+
+}
---
+++ Kowen/src/test/java/com/example/Kowen/KowenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Kowen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KowenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
