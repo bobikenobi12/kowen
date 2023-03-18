ca526a6956f09cf9026875dca6fcd7d2cdc58449
bobikenobi12 <borislav1207@gmail.com>
2023-03-18 22:13:48 +0200

    created root navbar and menu with account avatar and search bar

--- client/src/features/auth/authSlice.ts
+++ client/src/features/auth/authSlice.ts
@@ -4,30 +4,27 @@ import { User } from "../../app/api/types/index";
 const initialState: User = JSON.parse(localStorage.getItem("user") || "{}");
 
 const authSlice = createSlice({
-  name: "auth",
-  initialState,
-  reducers: {
-    setCredentials(state, action) {
-      const { id, first_name, last_name, email, avatar, _token } = action.payload;
-      state.id = id;
-      state.first_name = first_name;
-      state.last_name = last_name;
-      state.email = email;
-      state.avatar = avatar;
-      state._token = _token;
-    },
-  //   logOut(state) { 
-  //     state.id = "";
-  //     state.first_name = "";
-  //     state.last_name = "";
-  //     state.email = "";
-  //     state.avatar = "";
-  //     state._token = "";
-  // },
-  },
+	name: "auth",
+	initialState,
+	reducers: {
+		setCredentials(state, action) {
+			const { id, first_name, last_name, email, avatar, _token } =
+				action.payload;
+			state.id = id;
+			state.first_name = first_name;
+			state.last_name = last_name;
+			state.email = email;
+			state.avatar = avatar;
+			state._token = _token;
+		},
+		getCredentials(state) {
+			return state;
+		},
+	},
 });
 
 // will add logout if needed
 export const { setCredentials } = authSlice.actions;
+export const { getCredentials } = authSlice.actions;
 
 export default authSlice.reducer;
