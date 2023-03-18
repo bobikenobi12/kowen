117dda6be2c791a0da5996ac3ebf96505b661d9b
AntonStankov <antonstankov007@gmail.com>
2023-03-18 22:39:00 +0200

    database connection with user and role entities added alongside with relationship between them

---
+++ Kowen/src/main/java/com/example/Kowen/entity/User.java
@@ -0,0 +1,45 @@
+package com.example.Kowen.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String password;
+
+    @Column
+    private String username;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @JsonIgnore
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private List<Role> roles = new ArrayList<>();
+
+
+}
