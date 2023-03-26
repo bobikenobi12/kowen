efec4c8e2cc7c5ed1b5eb56fb39b74f9df046dec
bobikenobi12 <borislav1207@gmail.com>
2023-03-26 15:27:14 +0300

    added route guards

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,8 @@ import { Box, Center, VStack, Text } from "@chakra-ui/react";
 
 import { Login } from "./features/auth/Login";
 import { PrivateOutlet } from "./utils/PrivateOutlet";
-import { ProtectedComponent } from "./features/auth/ProtectedComponent";
-
+// import { ProtectedComponent } from "./features/auth/ProtectedComponent";
+import { PublicOutlet } from "./utils/PublicOutlet";
 // function Hooray() {
 // 	return (
 // 		<Center h="500px">
@@ -22,7 +22,9 @@ function App() {
 	return (
 		<Box>
 			<Routes>
-				<Route path="/login" element={<Login />} />
+				<Route element={<PublicOutlet />}>
+					<Route path="/login" element={<Login />} />
+				</Route>
 				<Route path="/" element={<PrivateOutlet />}>
 					<Route index element={<Text>Home</Text>} />
 				</Route>
