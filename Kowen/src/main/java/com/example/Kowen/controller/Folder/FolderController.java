5da0d9436a96bfb12aa567623bfb5c70280718d1
AntonStankov <antonstankov007@gmail.com>
2023-04-02 00:16:03 +0300

    added API to change folder name

--- Kowen/src/main/java/com/example/Kowen/controller/Folder/FolderController.java
+++ Kowen/src/main/java/com/example/Kowen/controller/Folder/FolderController.java
@@ -1,9 +1,8 @@
 package com.example.Kowen.controller.Folder;
 
-import com.example.Kowen.entity.Folder;
-import com.example.Kowen.entity.User;
-import com.example.Kowen.entity.UserGroup;
+import com.example.Kowen.entity.*;
 import com.example.Kowen.service.folder.FolderRepo;
+import com.example.Kowen.service.folder.FolderService;
 import com.example.Kowen.service.group.GroupRepo;
 import com.example.Kowen.service.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +24,9 @@ public class FolderController {
 
     @Autowired
     private FolderRepo folderRepo;
+
+    @Autowired
+    private FolderService folderService;
 
     @Autowired
     private GroupRepo groupRepo;
@@ -60,5 +62,23 @@ public class FolderController {
         if (!group.getUsers().contains(user) && group.getCreator() != user) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "You are not in this group!");
 
         return group.getFolders();
+    }
+
+    //================================Changes===================================
+
+    @PostMapping("/changeName/{groupId}/{folderId}")
+    public Folder changeName(@PathVariable Long groupId, @PathVariable Long folderId, @RequestParam String name) throws Exception {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails principal = (UserDetails) authentication.getPrincipal();
+        User user =  userRepository.findByEmail(principal.getUsername()).get(0);
+
+        UserGroup group = groupRepo.findById(groupId).orElseThrow(Exception::new);
+
+        for (Folder folder : group.getFolders()){
+            if (folder.getId() == folderId && group.getCreator() == user){
+                return folderService.changeName(folderId, name);
+            }
+        }
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "There is no such document!");
     }
 }
