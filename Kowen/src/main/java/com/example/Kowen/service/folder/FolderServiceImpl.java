ca06d8981f775eee5af7baa725d2b6ca8f609b84
AntonStankov <antonstankov007@gmail.com>
2023-04-06 15:12:20 +0300

    added deleteFolder API

--- Kowen/src/main/java/com/example/Kowen/service/folder/FolderServiceImpl.java
+++ Kowen/src/main/java/com/example/Kowen/service/folder/FolderServiceImpl.java
@@ -1,8 +1,14 @@
 package com.example.Kowen.service.folder;
 
 import com.example.Kowen.entity.Folder;
+import com.example.Kowen.entity.UserGroup;
+import com.example.Kowen.service.group.GroupRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 public class FolderServiceImpl implements FolderService{
@@ -10,10 +16,29 @@ public class FolderServiceImpl implements FolderService{
     @Autowired
     private FolderRepo folderRepo;
 
+    @Autowired
+    private GroupRepo groupRepo;
+
     @Override
     public Folder changeName(Long folderId, String name) throws Exception {
         Folder folder = folderRepo.findById(folderId).orElseThrow(Exception::new);
         folder.setName(name);
         return folderRepo.save(folder);
+    }
+
+    @Override
+    public Boolean deleteFolder(Long groupId, Long folderId) throws Exception {
+        UserGroup group = groupRepo.findById(groupId).orElseThrow(Exception::new);
+        Folder folder = folderRepo.findById(folderId).orElseThrow(Exception::new);
+
+        if (group.getFolders().contains(folder)){
+            List<Folder> folders = group.getFolders();
+            folders.remove(folder);
+            group.setFolders(folders);
+            folderRepo.delete(folder);
+            groupRepo.save(group);
+            return Boolean.TRUE;
+        }
+        else throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "There is no such folder in this group!");
     }
 }
