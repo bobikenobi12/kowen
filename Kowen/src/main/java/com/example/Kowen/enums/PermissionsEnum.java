1d93d54d829372b886333eaf08130076664bd360
AntonStankov <antonstankov007@gmail.com>
2023-04-12 16:03:53 +0300

    added new permissions

--- Kowen/src/main/java/com/example/Kowen/enums/PermissionsEnum.java
+++ Kowen/src/main/java/com/example/Kowen/enums/PermissionsEnum.java
@@ -4,5 +4,18 @@ public enum PermissionsEnum {
     can_view,
     can_edit,
     can_delete,
-    can_add
+    can_add,
+    add_role,
+    delete_role,
+    edit_role,
+    remove_user,
+    add_user,
+    edit_group,
+    add_folder,
+    delete_folder,
+    edit_folder,
+    add_document,
+    remove_document,
+    download_document,
+    save_new_document_version
 }
