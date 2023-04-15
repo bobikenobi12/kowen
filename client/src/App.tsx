37cb7beb29b600c8a9a9bdeb6f828d531865afa9
bobikenobi12 <borislav1207@gmail.com>
2023-04-15 14:15:12 +0300

    updated routing and added scroll to folders

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import {
+	BrowserRouter as Router,
 	Routes,
 	Route,
 	createBrowserRouter,
@@ -23,68 +24,94 @@ import Folder from "./features/folders/Folder";
 import UserSettings from "./features/auth/UserSettings";
 import GroupSettings from "./features/groups/GroupSettings/GroupSettings";
 
-const router = createBrowserRouter([
-	{
-		element: <PrivateOutlet />,
-		children: [
-			{
-				path: "/groups/",
-				element: <Groups />,
-				errorElement: <NotFound />,
-				children: [
-					{
-						path: ":groupId/",
-						element: <Group />,
-						children: [
-							{
-								path: "folders/:folderId",
-								element: <Folder />,
-							},
-						],
-					},
-				],
-			},
-			{
-				path: "/settings",
-				element: <UserSettings />,
-			},
-			{
-				path: "/groups/:groupId/settings",
-				element: <GroupSettings />,
-			},
-			{
-				path: "/404",
-				element: <NotFound />,
-			},
-			{
-				path: "*",
-				element: <NotFound />,
-			},
-		],
-	},
-	{
-		element: <PublicOutlet />,
-		children: [
-			{
-				path: "/",
-				element: <Hero />,
-			},
-			{
-				path: "/login",
-				element: <Login />,
-			},
-			{
-				path: "/register",
-				element: <Register />,
-			},
-		],
-	},
-]);
+// const router = createBrowserRouter([
+// 	{
+// 		element: <PrivateOutlet />,
+// 		children: [
+// 			{
+// 				path: "/groups/",
+// 				element: <Groups />,
+// 				errorElement: <NotFound />,
+// 				children: [
+// 					{
+// 						path: ":groupId/",
+// 						element: <Group />,
+// 						children: [
+// 							{
+// 								path: "folders/:folderId",
+// 								element: <Folder />,
+// 							},
+// 						],
+// 					},
+// 				],
+// 			},
+// 			{
+// 				path: "/settings",
+// 				element: <UserSettings />,
+// 			},
+// 			{
+// 				path: "/groups/:groupId/settings",
+// 				element: <GroupSettings />,
+// 			},
+// 			{
+// 				path: "/404",
+// 				element: <NotFound />,
+// 			},
+// 			{
+// 				path: "*",
+// 				element: <NotFound />,
+// 			},
+// 		],
+// 	},
+// 	{
+// 		element: <PublicOutlet />,
+// 		children: [
+// 			{
+// 				path: "/",
+// 				element: <Hero />,
+// 			},
+// 			{
+// 				path: "/login",
+// 				element: <Login />,
+// 			},
+// 			{
+// 				path: "/register",
+// 				element: <Register />,
+// 			},
+// 		],
+// 	},
+// ]);
 
 function App() {
 	return (
 		<Box>
-			<RouterProvider router={router} />
+			<Router>
+				<Routes>
+					<Route element={<PrivateOutlet />}>
+						<Route path="groups" element={<Groups />}>
+							<Route path=":groupId" element={<Group />}>
+								<Route
+									path="folders/:folderId"
+									element={<Folder />}
+								/>
+								<Route
+									path="settings"
+									element={<GroupSettings />}
+								/>
+							</Route>
+						</Route>
+						<Route path="settings" element={<UserSettings />} />
+						<Route path="404" element={<NotFound />} />
+						<Route path="*" element={<NotFound />} />
+					</Route>
+
+					<Route element={<PublicOutlet />}>
+						<Route path="/" element={<Hero />} />
+						<Route path="login" element={<Login />} />
+						<Route path="register" element={<Register />} />
+					</Route>
+				</Routes>
+			</Router>
 		</Box>
 	);
 }
